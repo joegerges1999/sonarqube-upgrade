a427dcee0aba88005c29c5ac151cebfd2336246f
GERGES Joe <jgerges@murex.com>
2020-08-14 10:59:59 +0300

    init

--- upgrade.sh
+++ upgrade.sh
@@ -1,31 +1,42 @@
-team=$1
-upgrade_to=$2
-cluster_id=c-jpxcn
-project_id=p-zwxgj
+TEAM=$1
+UPGRADE_TO=$2
+APP=$3
+CLUSTER_ID=c-jpxcn
+PROJECT_ID=p-zwxgj
+
+if [[ -z $TEAM || -z $UPGRADE_TO || -z $APP ]]; then
+  echo 'One or more variables are undefined, exiting script ...'
+  exit 1
+fi
 
 echo "Fetching metadata ..."
-HOSTNAME=$(kubectl get --all-namespaces ingress -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.rules[0].host}")
-WEBCONTEXT=$(kubectl get --all-namespaces ingress -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.rules[0].http.paths[0].path}")
-POD=$(kubectl get pod --all-namespaces -l app=sonarqube,team=$team -o jsonpath="{.items[0].metadata.name}")
-current_version=$(kubectl get --all-namespaces deployment -l app=sonarqube,team=$team -o jsonpath="{.items[0].spec.template.spec.containers[1].image}")
-current_version=$(cut -d ":" -f2 <<< "$current_version")
+HOSTNAME=$(kubectl get --all-namespaces ingress -l app=$APP,team=$TEAM -o jsonpath="{.items[0].spec.rules[0].host}")
+WEBCONTEXT=$(kubectl get --all-namespaces ingress -l app=$APP,team=$TEAM -o jsonpath="{.items[0].spec.rules[0].http.paths[0].path}")
+POD=$(kubectl get pod --all-namespaces -l app=$APP,team=$TEAM -o jsonpath="{.items[0].metadata.name}")
+CURRENT_VERSION=$(kubectl get --all-namespaces deployment -l app=$APP,team=$TEAM -o jsonpath="{.items[0].spec.template.spec.containers[1].image}")
+CURRENT_VERSION=$(cut -d ":" -f2 <<< "$CURRENT_VERSION")
+
+if [[ -z $HOSTNAME || -z $WEBCONTEXT || -z $POD || -z $CURRENT_VERSION ]]; then
+  echo 'One or more variables could not be fetched, exiting script ...'
+  exit 1
+fi
 
 echo "Logging in to rancher ..."
-rancher login https://rancher.cd.murex.com/ --token token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r --context $cluster_id:$project_id
-app_version=$(rancher app | grep $team-sonarqube | awk '{print $6}')
+rancher login https://rancher.cd.murex.com/ --token token-vkq9d:wg8gtt4gbgtk7nzfhlj4gs87dn4w2hxhd9qmcb9fmnqkllgx57792r --context $CLUSTER_ID:$PROJECT_ID
+APP_VERSION=$(rancher app | grep $TEAM-$APP | awk '{print $6}')
 
 echo "Removing ingress ..."
-rancher app upgrade $team-sonarqube $app_version --set ingress.enabled='false' --set sonarqube.image.tag="$current_version" --set hostname="$HOSTNAME" --set team="$team"
+rancher app upgrade $TEAM-$APP $APP_VERSION --set ingress.enabled='false' --set $APP.image.tag="$CURRENT_VERSION" --set hostname="$HOSTNAME" --set team="$TEAM"
 
 echo "Backing up database ..."
-kubectl -n sonarqube exec $POD -c sonardb -- bash -c "pg_dump -U sonar sonar > /var/lib/postgresql/data/sonar-backup.sql"
+kubectl -n $APP exec $POD -c sonardb -- bash -c "pg_dump -U sonar sonar > /var/lib/postgresql/data/sonar-backup.sql"
 
 echo "Waiting for 5 seconds..."
 sleep 5s
 
 echo "Deploying the upgrade ..."
-rancher app upgrade $team-sonarqube $app_version --set ingress.enabled='true' --set sonarqube.image.tag="$upgrade_to-community" --set hostname="$HOSTNAME" --set team="$team"
+rancher app upgrade $TEAM-$APP $APP_VERSION --set ingress.enabled='true' --set $APP.image.tag="$UPGRADE_TO-community" --set hostname="$HOSTNAME" --set team="$TEAM"
 
 echo "Initiating playbook ..."
 echo "http://$HOSTNAME$WEBCONTEXT/setup"
-ansible-playbook /data/jgerges/helm-charts/sonarqube/upgrade/upgrade.yaml --extra-vars "web_context=$WEBCONTEXT hostname=$HOSTNAME"
+ansible-playbook /data/jgerges/helm-charts/$APP/upgrade/upgrade.yaml --extra-vars "web_context=$WEBCONTEXT hostname=$HOSTNAME"
